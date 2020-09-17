69855a47eb95a64266dc5f98080e39d3fbd49729
Syn-Tax <twocap06@gmail.com>
2020-09-17 21:36:14 +0100

    Automated Commit

--- school/cs/Internal Assessment/.auctex-auto/test.el
+++ school/cs/Internal Assessment/.auctex-auto/test.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,10 +31,9 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org35e3539"
-    "sec:org4c24a78")
+    "sec:org65ab663"
+    "sec:org531805b")
    (LaTeX-add-bibliographies
-    "bibliography"
-    "bibliography.bib"))
+    "bibliography"))
  :latex)
 
